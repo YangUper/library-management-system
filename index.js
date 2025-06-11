599d4b0f600f4de120eff0366baeb69c43dda825
YangUper <1792543295@qq.com>
2025-06-11 19:30:34 +0800

    实现修改图书信息

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const creator = 'kt'
-
+// 渲染
 function getBookList() {
     axios({
         url: 'https://hmajax.itheima.net/api/books',
@@ -12,7 +12,7 @@ function getBookList() {
         bookList = result.data.data
         const htmlStr = bookList.map((item, id) => {
             return `<tr>
-                    <td>${id+1}</td>
+                    <td>${id + 1}</td>
                     <td>${item.bookname}</td>
                     <td>${item.author}</td>
                     <td>${item.publisher}</td>
@@ -25,14 +25,13 @@ function getBookList() {
         document.querySelector('.list').innerHTML = htmlStr
     })
 }
-
 getBookList()
-
+// 增加
 const addModalDom = document.querySelector('.add-modal')
 const addModal = new bootstrap.Modal(addModalDom)
 document.querySelector('.saveBtn').addEventListener('click', () => {
     const addForm = document.querySelector('.addBookForm')
-    const bookObj = serialize(addForm, {hash: true, empty: true})
+    const bookObj = serialize(addForm, { hash: true, empty: true })
     console.log(bookObj)
 
     axios({
@@ -50,13 +49,13 @@ document.querySelector('.saveBtn').addEventListener('click', () => {
     })
 
 })
-
+// 删除
 document.querySelector('.list').addEventListener('click', e => {
     // console.log(e.target)
-    if (e.target.classList.contains('del')){
+    if (e.target.classList.contains('del')) {
         // console.log('点击删除元素')
         theId = e.target.parentNode.dataset.id
-        
+
         axios({
             url: `https://hmajax.itheima.net/api/books/${theId}`,
             method: 'delete'
@@ -64,4 +63,46 @@ document.querySelector('.list').addEventListener('click', e => {
             getBookList()
         })
     }
+})
+// 编辑
+const editDom = document.querySelector('.edit-modal')
+const editModal = new bootstrap.Modal(editDom)
+document.querySelector('.list').addEventListener('click', e => {
+    if (e.target.classList.contains('edit')) {
+        theId = e.target.parentNode.dataset.id
+        axios({
+            url: `https://hmajax.itheima.net/api/books/${theId}`
+        }).then(result => {
+            // console.log(result)
+            const bookObj = result.data.data
+            // document.querySelector('.editBookForm .bookname').value = bookObj.bookname
+            // document.querySelector('.editBookForm .author').value = bookObj.author
+            // document.querySelector('.editBookForm .publisher').value = bookObj.publisher
+            const keys = Object.keys(bookObj)
+            // console.log(keys)   ['id', 'bookname', 'author', 'publisher']
+            keys.forEach(key => {
+                const input = document.querySelector(`.editBookForm .${key}`);
+                if (input) input.value = bookObj[key];
+            });
+        })
+        editModal.show()
+    }
+})
+document.querySelector('.editBtn').addEventListener('click', () => {
+    const editForm = document.querySelector('.editBookForm')
+    const { id, bookname, author, publisher } = serialize(editForm, { hash: true, empty: true })
+    // console.log(bookObj)
+    axios({
+        url: `https://hmajax.itheima.net/api/books/${id}`,
+        method: 'put',
+        data: {
+            bookname,
+            author,
+            publisher,
+            creator
+        }
+    }).then(() => {
+        getBookList()
+        editModal.hide()
+    })
 })
